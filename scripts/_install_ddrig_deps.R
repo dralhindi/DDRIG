0ced22c99b2e45430df4ddaa38555ca3f822f2d9
Naji <naji.sawas.contact@gmail.com>
2020-11-26 15:14:20 -0800

    yay

---
+++ scripts/_install_ddrig_deps.R
@@ -0,0 +1,22 @@
+# If a package is installed, it will be loaded. If any are not, the missing package(s) will be installed
+# and then loaded.
+
+r_packages <- c("dplyr",
+              "tidyverse",
+              "ggplot2",
+              "ggrepel",
+              "NCmisc",
+              "lintr",
+              "precommit",
+              "ggforce") # ggforce tends to have issues installing due to RcppEigen
+
+missing_r_packages <- r_packages[!(r_packages %in% installed.packages()[, "Package"])]
+if (length(missing_r_packages)) install.packages(missing_r_packages, repo = "http://cran.rstudio.com/")
+
+github_packages <- c("precommit")
+
+missing_gh_packages <- github_packages[!(github_packages %in% installed.packages()[, "Package"])]
+lapply(missing_gh_packages, remotes::install_github)
+
+# Initialize precommit.
+precommit::use_precommit()
--- scripts/install_ddrig_deps.sh
+++ scripts/install_ddrig_deps.sh
@@ -2,37 +2,12 @@
 
 if ! pre-commit --version &> /dev/null
 then
-    echo "pre-commit is not installed. Please install pre-commit using pip or brew."
+    echo "pre-commit is not installed. Please install pre-commit using pip or brew.\n"
     exit
 fi
 
-
-
-# Standard R Packages.
-declare -a cran_packages=(
-	"ggforce"
-	"dplyr"
-	"tidyverse"
-	"ggplot2"
-	"ggrepel"
-	"NCmisc"
-)
-
 echo "Installing R Packages..."
-for pkg in "${cran_packages[@]}"; do
-	Rscript -e "if (!'$pkg' %in% rownames(installed.packages())) install.packages('$pkg', repo='http://cran.rstudio.com/')"
-done
 
-
-# Github packages.
-declare -a github_packages=(
-	"lorenzwalthert/precommit"
-)
-
-
-echo "Installing GitHub Packages..."
-for pkg in "${github_packages[@]}"; do
-	Rscript -e "if (!'$pkg' %in% rownames(installed.packages())) remotes::install_github('$pkg')"
-done
+time Rscript -e 'source("./scripts/_install_ddrig_deps.R")'
 
 echo "Installation complete!"

b0fa9d440103339be7a8c6529b7b08791c7c9b8d
dralhindi <dralhindi@ucdavis.edu>
2020-12-08 19:22:33 -0800

    updating boxplot code

--- Pheno/scripts/khoesan_pigmentation_histogram_script.R
+++ Pheno/scripts/khoesan_pigmentation_histogram_script.R
@@ -11,6 +11,11 @@ master <- read.csv("Pheno/data_private/MasterPheno_All.csv")
 head(master)
 class(master)
 
+ced <- read.csv("Pheno/data_private/Master_Pheno_CDB.csv")
+karoo <- read.csv("Pheno/data_private/Master_Pheno_Karoo.csv")
+
+
+
 # Remove all NA values
 master <- master[-which(is.na(as.numeric(master$M_Arm))), ]
 
@@ -38,6 +43,17 @@ histogram <- ggplot(master, aes(x = M_Arm)) +
   theme_set(theme_classic() + theme(legend.position = "top")
   )
 
+ggplot(ced, aes(x = Final_M)) +
+  geom_histogram(binwidth = 0.75, fill = "gray", color = "gray", alpha = 0.9) +
+  geom_vline(aes(xintercept = cdb_avg), col = "red", size = 0.75) +
+  theme_set(theme_classic() + theme(legend.position = "top")
+  )
+
+ggplot(karoo, aes(x = Final_M)) +
+  geom_histogram(binwidth = 0.75, fill = "gray", color = "gray", alpha = 0.9) +
+  geom_vline(aes(xintercept = karoo_avg), col = "red", size = 0.75) +
+  theme_set(theme_classic() + theme(legend.position = "top")
+  )
 
 
 # Plot Bar Plot
@@ -46,29 +62,31 @@ head(master)
 master$Population <- with(master, reorder(Population, M_Arm, median))
 
 
-barplot <- ggplot(master, aes(fill = Population, x = Population, y = M_Arm)) +
-  geom_boxplot(aes(show.legend = FALSE, alpha = 0.8)) +
+boxplot1 <-
+  ggplot(master, aes(x = Population, y = M_Arm)) +
+  geom_boxplot(aes(show.legend = FALSE), fill = c("#9A872D", "#F7B0AA", "#FDDDA4", "#76A08A"), notch = TRUE,
+               outlier.colour = "#D1362F", outlier.alpha = 0.5) +
   labs(title = "Melanin Distribution in KhoeSan Populations", y = "Melanin",
        caption = "Source: Henn Lab pigmentation data") +
   scale_color_brewer(palette = "Dark2") +
   theme(legend.position = "none")
 
 
-
-
-# Plot Violin Plot
-violinplot <- ggplot(master, aes(fill = Population, x = Population, y = M_Arm)) +
-  geom_violin(aes(show.legend = FALSE, alpha = 0.8)) +
-  labs(title = "Melanin Distribution in KhoeSan Populations", y = "Melanin Content",
+boxplot2 <-
+  ggplot(master, aes(x = Population, y = M_Arm)) +
+  geom_boxplot(aes(show.legend = FALSE), fill = c("#9A872D", "#F7B0AA", "#FDDDA4", "#76A08A"), notch = TRUE,
+               outlier.colour = "#D1362F", outlier.alpha = 0.5) +
+  geom_point(alpha = 0.02) + geom_jitter(width = 0.18, color = "#541F12", shape = 1) +
+  labs(title = "Melanin Distribution in KhoeSan Populations", y = "Melanin",
        caption = "Source: Henn Lab pigmentation data") +
   scale_color_brewer(palette = "Dark2") +
   theme(legend.position = "none")
-
+?geom_jitter
 
 
 
 # Save File
 pdf(file = "Pheno/output/distribution_pigmentation_plot.pdf", width = 12, height = 9)
-barplot
-violinplot
+boxplot1
+boxplot2
 dev.off()

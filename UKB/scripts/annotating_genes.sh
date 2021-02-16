325ae9a3f8db1083cf2ad8ac7ebc5578a8600cbb
dralhindi <62627039+dralhindi@users.noreply.github.com>
2021-02-16 14:30:47 -0800

    Update annotating_genes.sh

--- UKB/scripts/annotating_genes.sh
+++ UKB/scripts/annotating_genes.sh
@@ -3,8 +3,8 @@
 module load bedtools2/2.29.2
 
 ##Get files
-cp /share/hennlab/projects/UKB_Pigmentation/01-Working/d_workspace/glist-hg19 .
-cp -s /share/hennlab/projects/UKB_Pigmentation/01-Working/d_workspace/continuous-1717-both_sexes.metaonly.tsv .
+cp /share/hennlab/projects/UKB_Pigmentation/01-Working/d_workspace/glist-hg19 # file with gene names and ranges
+cp -s /share/hennlab/projects/UKB_Pigmentation/01-Working/d_workspace/continuous-1717-both_sexes.metaonly.tsv # file I want to annotate 
 
 ##Make bed file of SNPs (tail to remove header, awk to format as chr, start, stop)
 tail -n +2 continuous-1717-both_sexes.metaonly.tsv | awk '{print ($1"\t"$2"\t"$2) }' > Variants.bed
@@ -12,7 +12,7 @@ tail -n +2 continuous-1717-both_sexes.metaonly.tsv | awk '{print ($1"\t"$2"\t"$2
 ##Reformat glist file to be tab delimited
 awk '{print ($1"\t"$2"\t"$3"\t"$4) }' glist-hg19 > glist-hg19.bed
 
-##Output of format chr, snp position, geneID
+##Output of format chr, snp position, geneID 
 bedtools intersect -a Variants.bed -b glist-hg19.bed -wb | cut -f 1,2,7 > AnnotatedSNPs.txt
 
 
